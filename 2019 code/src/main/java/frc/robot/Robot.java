1b263138bd8eb7bdedc14842e190643b0ea576e1
Ryan <ryan@thebacons.us>
2019-01-17 20:06:33 -0700

    Created RobotMap file
    Created RobotMap file and made various changes to use the new definitions

--- 2019 code/src/main/java/frc/robot/Robot.java
+++ 2019 code/src/main/java/frc/robot/Robot.java
@@ -2,11 +2,8 @@ package frc.robot;
 
 //IMPORTS
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PWMVictorSPX;                    //NOT USED
 import edu.wpi.first.wpilibj.TimedRobot;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.ControlMode;             //NOT USED
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;   //NOT WORKING
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
@@ -14,41 +11,22 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
-
+import frc.robot.RobotMap;
 
 //START ROBOT CLASS
 public class Robot extends TimedRobot {
-  //DEFINITIONS - WILL EVENTUALLY BE MADE INTO A ROBOT MAP FILE
-    //MOTOR MAP
-  private static final int rMotorPort = 1;
-  private static final int lMotorPort = 2;
-  private static final int lslavePort = 2;
-  private static final int rslavePort = 2;
 
+  //DEFINE MOTORS
+  WPI_TalonSRX rmotor = new WPI_TalonSRX(RobotMap.rMotorPort);
+  WPI_TalonSRX lmotor = new WPI_TalonSRX(RobotMap.lMotorPort);
+  WPI_TalonSRX lslave = new WPI_TalonSRX(RobotMap.lslavePort);
+  WPI_TalonSRX rslave = new WPI_TalonSRX(RobotMap.rslavePort);
 
-
-    //JOYSTICK MAP
-  private static final int kJoystickPort = 0;
-  private static final int leftX = 0;
-  private static final int leftY = 1;  
-  private static final int rightX = 4;  
-  private static final int rightY = 5;
-  private static final int buttonA = 1;
-  private static final int buttonB = 2;
-  private static final int buttonX = 3;
-
-  double scaler = 0.5;
-  Boolean buttonflag = false;
-
-  //  
-  WPI_TalonSRX rmotor = new WPI_TalonSRX(rMotorPort);
-  WPI_TalonSRX lmotor = new WPI_TalonSRX(lMotorPort);
-  WPI_TalonSRX lslave = new WPI_TalonSRX(lslavePort);
-  WPI_TalonSRX rslave = new WPI_TalonSRX(rslavePort);
-
+  //CREATE SPEED CONTROLLER GROUPS
   SpeedControllerGroup mleft = new SpeedControllerGroup(lmotor, lslave);
   SpeedControllerGroup mright = new SpeedControllerGroup(rmotor, rslave);
 
+  //INITIALIZE ARCADE DRIVE
   DifferentialDrive drive = new DifferentialDrive(mleft, mright);
 
   //VISION STUFF
@@ -68,13 +46,13 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
-    m_joystick = new Joystick(kJoystickPort);
+    m_joystick = new Joystick(RobotMap.kJoystickPort);
   } //END ROBOT INIT
 
   @Override
   public void teleopPeriodic() {
 
-    drive.arcadeDrive(m_joystick.getRawAxis(leftY), m_joystick.getRawAxis(leftX));
+    drive.arcadeDrive(-m_joystick.getRawAxis(RobotMap.leftY)*RobotMap.scaler, m_joystick.getRawAxis(RobotMap.leftX)*RobotMap.scaler);
 
   } //END ROBOTOT TELEOP
 } //END ROBOT CLASS
---
+++ 2019 code/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,31 @@
+package frc.robot;
+
+public class RobotMap {
+
+    //MOTOR DECLARATIONS
+    public static final int rMotorPort = 3;
+    public static final int rslavePort = 5;
+
+    public static final int lMotorPort = 2;
+    public static final int lslavePort = 4;
+
+    //GAMEPAD DECLARATIONS
+    public static final int kJoystickPort = 0;
+
+    public static final int leftX = 0;
+    public static final int leftY = 1;  
+    public static final int rightX = 4;  
+    public static final int rightY = 5;
+    public static final int rightTrigger = 3;
+    public static final int leftTrigger = 2;
+
+    public static final int buttonA = 1;
+    public static final int buttonB = 2;
+    public static final int buttonX = 3;
+    public static final int buttonY = 4;
+    public static final int lBumper = 5;
+    public static final int rBumper = 6;
+
+    //OTHER DECLARATIONS
+    public static final double scaler = 0.75;
+}
