85321615b29285875737adfc471a247ba4fc275e
Arjun624 <arjunavinash0@gmail.com>
2019-01-19 13:32:55 -0700

    Update MotorControl.java

--- 2019 code/src/main/java/frc/robot/MotorControl.java
+++ 2019 code/src/main/java/frc/robot/MotorControl.java
@@ -22,7 +22,7 @@ public class MotorControl{
         }
       } else {  //CONTROL MODE CHECK
         if(!Robot.oi.defaultDrivemode){
-          Robot.oi.drive.arcadeDrive(-Robot.oi.ljoystick.getRawAxis(RobotMap.joyY)*RobotMap.scaler, -Robot.oi.ljoystick.getRawAxis(RobotMap.joyX)*RobotMap.scaler);
+          Robot.oi.drive.arcadeDrive(-Robot.oi.ljoystick.getRawAxis(RobotMap.joyY)*RobotMap.scaler, Robot.oi.ljoystick.getRawAxis(RobotMap.joyX)*RobotMap.scaler);
         } else {
           Robot.oi.drive.tankDrive(-Robot.oi.ljoystick.getRawAxis(RobotMap.joyY)*RobotMap.scaler, -Robot.oi.rjoystick.getRawAxis(RobotMap.joyY)*RobotMap.scaler);
         }
