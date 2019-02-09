9eee6d95841b94508cdb880a0d0b4d928e17e5df
etc3po <46801109+etc3po@users.noreply.github.com>
2019-02-08 20:36:12 -0700

    2nd version

--- 2019 code/src/main/java/frc/robot/subsystems/RobotLift.java
+++ 2019 code/src/main/java/frc/robot/subsystems/RobotLift.java
@@ -6,35 +6,51 @@ import frc.robot.robotmain.*;
 public class RobotLift{
     public RobotLift(){
        
- double targetDistance = -2;      //THIS IS IN FEET
+ double targetDistance = -2;       //THIS IS IN FEET
+ double targetBoi = .21; //sample
+ double targetDistance2 = 1;
+
 
  {
     int stagecounter=1;
      switch(stagecounter){
         case 1:
-            if(-Robot.oi.rmotor1.getSelectedSensorPosition() / -Robot.globalVariables.oneFootRightEncoder*targetDistance < 100 && Robot.oi.lmotor1.getSelectedSensorPosition() / Robot.globalVariables.oneFootLeftEncoder*targetDistance < 100){
-                if(-Robot.oi.rmotor1.getSelectedSensorPosition() / -Robot.globalVariables.oneFootRightEncoder*targetDistance > Robot.oi.lmotor1.getSelectedSensorPosition() / Robot.globalVariables.oneFootLeftEncoder*targetDistance){
-                    Robot.oi.drive.tankDrive(Robot.globalVariables.autonomousSpeed -.3, -Robot.globalVariables.autonomousSpeed +.1);            
-                } else {
-                    Robot.oi.drive.tankDrive(Robot.globalVariables.autonomousSpeed +.1 , -Robot.globalVariables.autonomousSpeed -.3);
-                }
-            } else {
+            if(Robot.oi.rmotor1.getSelectedSensorPosition()>targetDistance*Robot.globalVariables.oneFootRightEncoder && Robot.oi.lmotor1.getSelectedSensorPosition()>targetDistance*Robot.globalVariables.oneFootLeftEncoder){
+                Robot.oi.drive.tankDrive(-.6,-.6);
+            } 
+            else{
                 Robot.oi.drive.tankDrive(0, 0);
+                stagecounter++;
+                break;
             }
-            stagecounter++;
-            break;
+            
+           
 
-            case 2:
+        case 2:
             Robot.oi.rLift.set(.6);
-            stagecounter++;
-            break;
+            if(Robot.oi.rLift.getSelectedSensorPosition()<targetBoi){
+                Robot.oi.rLift.set(.6);
+            }
+            else{
+                Robot.oi.rLift.set(0); 
 
-            case 3:
-                 Robot.oi.drive.tankDrive(.6, .6);
-                 stagecounter++;
-                 break;
-
-                 default:
+                stagecounter++;
+                break;
+            }
+        
+            if(Robot.globalVariables.rliftencoder/targetBoi>.5) {
+                Robot.oi.drive.tankDrive(.6, .6);
+            }
+        case 3:
+            if(Robot.oi.rmotor1.getSelectedSensorPosition()<targetDistance2*Robot.globalVariables.oneFootRightEncoder && Robot.oi.lmotor1.getSelectedSensorPosition()<targetDistance2*Robot.globalVariables.oneFootLeftEncoder ){
+                Robot.oi.drive.tankDrive(.6, .6);  
+            }
+            else{
+                Robot.oi.drive.tankDrive(0, 0);
+                stagecounter++;
+                break;
+            }
+        default:
                  Robot.oi.drive.tankDrive(0, 0);
     }
 }
