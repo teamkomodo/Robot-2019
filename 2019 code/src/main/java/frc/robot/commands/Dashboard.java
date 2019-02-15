215e218e8ea9c29004f2cc8d38046e8dc39fc725
GhostRunner21 <42250543+GhostRunner21@users.noreply.github.com>
2019-02-14 17:56:41 -0700

    Update Dashboard.java

--- 2019 code/src/main/java/frc/robot/commands/Dashboard.java
+++ 2019 code/src/main/java/frc/robot/commands/Dashboard.java
@@ -6,7 +6,7 @@ public class Dashboard{
     public Dashboard() {
         SmartDashboard.putNumber("Gyro 1", Robot.oi.gyro.getAngleZ());
         SmartDashboard.putNumber("Gyro 2", Robot.oi.gyro2.getAngle());
-\        SmartDashboard.putNumber("Left Y", Robot.oi.ljoystick.getRawAxis(RobotMap.joyY));
+        SmartDashboard.putNumber("Left Y", Robot.oi.ljoystick.getRawAxis(RobotMap.joyY));
         SmartDashboard.putNumber("Right Y", Robot.oi.rjoystick.getRawAxis(RobotMap.joyY));
     }
 }
