c28a45131452d40850818867217547120cecb5de
GhostRunner21 <42250543+GhostRunner21@users.noreply.github.com>
2019-03-01 20:19:33 -0700

    Update Elevator.java

--- 2019 code/src/main/java/frc/robot/commands/Elevator.java
+++ 2019 code/src/main/java/frc/robot/commands/Elevator.java
@@ -21,9 +21,9 @@ public class Elevator{
         //     }
         // }
         if(Robot.oi.gamepad.getRawAxis(RobotMap.leftY) > .2){
-            Robot.oi.mLift1.set(Robot.oi.gamepad.getRawAxis(RobotMap.leftY) * .8);
+            Robot.oi.mLift1.set(Robot.oi.gamepad.getRawAxis(RobotMap.leftY) * .1);
         }else if(Robot.oi.gamepad.getRawAxis(RobotMap.leftY) < -.2){
-            Robot.oi.mLift1.set(Robot.oi.gamepad.getRawAxis(RobotMap.leftY) * .2);
+            Robot.oi.mLift1.set(Robot.oi.gamepad.getRawAxis(RobotMap.leftY) * .7);
         } else{
             Robot.oi.mLift1.set(-.15);
         }
